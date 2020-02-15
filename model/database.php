aec572e6c9d487a2f010242adb7959318116e7e2
tiendadelmotero <61068635+tiendadelmotero@users.noreply.github.com>
2020-02-14 22:09:24 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=prueba_cotizacion;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=remotemysql.com;dbname=HRme7q7ZMd;charset=utf8', 'HRme7q7ZMd', 'SHriNyNfWv');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
